b0e765682b409e961cc6a4e349c6392b4c8e808a
XYZ0101 <72097776+gdasv0101@users.noreply.github.com>
2022-10-30 01:51:44 +0530

    Update Python program to draw hexagon.py

--- Python program to draw hexagon.py
+++ Python program to draw hexagon.py
@@ -5,11 +5,11 @@
 import turtle
 polygon = turtle.Turtle()
 
-num_sides = 6
+num_sides = 6 
 side_length = 70
-angle = 360.0 / num_sides
+angle = 360.0 / num_sides 
 
-for i in range(num_sides):
+for i in range(num_sides): #loop
 	polygon.forward(side_length)
 	polygon.right(angle)
 	
