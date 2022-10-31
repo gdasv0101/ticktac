af85c94da0b0e1d7a93a0b8d73bd7aee7c35a038
Josel099 <115106938+Josel099@users.noreply.github.com>
2022-10-31 19:40:23 +0530

    Update Python program to draw hexagon.py
    to draw hexagon usoing python

--- Python program to draw hexagon.py
+++ Python program to draw hexagon.py
@@ -1,12 +1,12 @@
 # Python program to draw hexagon
 # using Turtle graphics
 # basic turtle syntax
-
+#hexagon
 import turtle
 polygon = turtle.Turtle()
 
-num_sides = 6 
-side_length = 70
+num_sides = 6 #specifying the number of sides
+side_length = 70 #specifying the length of one side
 angle = 360.0 / num_sides 
 
 for i in range(num_sides): #loop
